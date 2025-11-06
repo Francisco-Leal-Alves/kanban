a43967047c27ec5b8494136c0585612cd328a211
Francisco-Leal-Alves <franciscolealalves2020@gmail.com>
2025-11-06 15:02:58 -0300

    Primeira

---
+++ app/src/main/java/com/francisco/task/ui/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.francisco.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.francisco.task.R
+import com.francisco.task.databinding.FragmentSplashBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class SplashFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+}
