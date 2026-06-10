231b9c2bfb6cc8bf5bdc442cd4aa509352928ddb
Francisco Leal <franciscolealalves2020@gmail.com>
2026-06-10 00:00:39 -0300

    terminado

--- app/src/main/java/com/francisco/task/ui/DoingFragment.kt
+++ app/src/main/java/com/francisco/task/ui/DoingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.francisco.task.R
@@ -37,6 +38,23 @@ class DoingFragment : Fragment() {
     private fun initListeners() {
         binding.floatingActionButton2.setOnClickListener {
             findNavController().navigate((R.id.action_homeFragment_to_formTaskFragment))
+        }
+    }
+
+    private fun optionSelected(task:Task, option:Int){
+        when (option){
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_NEXT -> {
+                Toast.makeText(requireContext(), "Próximo", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
--- app/src/main/java/com/francisco/task/ui/DoneFragment.kt
+++ app/src/main/java/com/francisco/task/ui/DoneFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.francisco.task.R
@@ -40,6 +41,22 @@ class DoneFragment : Fragment() {
         }
     }
 
+    private fun optionSelected(task:Task, option:Int){
+        when (option){
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_NEXT -> {
+                Toast.makeText(requireContext(), "Próximo", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     private fun getTask() = listOf(
         Task(id = "0", description = "Criar nova tela do app", Status.TODO),
         Task(id = "1", description = "Validar informações na tela de login", Status.TODO),
